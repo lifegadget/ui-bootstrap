7826c6208b122289e16bec37ac39d16a7a18ee23
Ken Snyder <ken@ken.net>
2016-02-16 09:58:14 +0000

    bump npm

--- tests/dummy/app/controllers/components.js
+++ tests/dummy/app/controllers/components.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 const htmlSafe = Ember.String.htmlSafe;
 
 export default Ember.Controller.extend({
+  inverse: false,
   actions: {
     cardAction(context) {
       this.get('flashMessages').success(htmlSafe(`Action button -- <i>${context.elementId}</i> -- was pressed`));
